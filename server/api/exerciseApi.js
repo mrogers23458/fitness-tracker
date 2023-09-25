6c924a5e9929e41ab182bc5e1670056236ce5484
iburley33 <iburley33@gmail.com>
2023-09-24 20:05:11 -0400

    updated exercise API

--- server/api/exerciseApi.js
+++ server/api/exerciseApi.js
@@ -1,5 +1,6 @@
 // Modules
 const express = require("express");
+const mongoose = require("mongoose");
 const exerciseApi = express.Router();
 
 // Models
@@ -7,11 +8,46 @@ const Exercise = require("../models/Exercise.js");
 const User = require("../models/User.js");
 
 // Get Route All
-exerciseApi.get("/", (req, res) => {});
+exerciseApi.get("/", async (req, res) => {
+  try {
+    // Pagination parameters
+    const page = req.query.page || 1;
+    const limit = req.query.limit || 10; // Default limit is 10
+
+    const skip = (page - 1) * limit; // if page is 1, skip is 0??
+
+    // Query the database with pagination
+    const exercises = await Exercise.find().skip(skip).limit(limit);
+
+    res.status(200).json(exercises);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Internal Server Error." });
+  }
+});
 
 // Get Route One
-exerciseApi.get("/:id", (req, res) => {
+exerciseApi.get("/:id", async (req, res) => {
   const id = req.params.id;
+  
+  // Validate if the 'id' is a valid ObjectId
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res
+      .status(400)
+      .json({ success: false, message: "Invalid exercise ID format." });
+  }
+
+  try {
+    const foundExercise = await Exercise.findById(id);
+    if (!foundExercise) {
+      res.status(404).json({ message: "No exercise found." });
+    } else {
+      res.status(200).json(foundExercise);
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Internal Server Error." });
+  }
 });
 
 // Create Route
@@ -40,14 +76,52 @@ exerciseApi.post("/", async (req, res) => {
 });
 
 // Update One Route
-exerciseApi.put("/:id", (req, res) => {
-  const id = req.params.id;
+exerciseApi.put("/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const update = req.body;
+
+    // Check if the request body is empty or missing fields (you can customize this validation)
+    if (!update || Object.keys(update).length === 0) {
+      return res.status(400).json({ error: "Invalid update data" });
+    }
+
+    const updatedExercise = await Exercise.findByIdAndUpdate(id, update, {
+      new: true,
+    });
+
+    // Check if the user with the specified ID was found and updated
+    if (!updatedExercise) {
+      return res.status(404).json({ error: "Exercise not found" });
+    }
+    res.status(200).json(updatedExercise);
+  } catch (error) {
+    console.error("Error updating exercise:", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
 });
 
 // Delete One Route
 
-exerciseApi.delete("/:id", (req, res) => {
-  const id = req.params.id;
+exerciseApi.delete("/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+
+    // Perform the deletion and get the result
+    const deletedExercise = await Exercise.findByIdAndRemove(id);
+
+    if (!deletedExercise) {
+      return res.status(404).json({ error: "Exercise not found" });
+    }
+
+    // 204 No Content indicates successful deletion with no response body
+    res.status(204).send();
+  } catch (error) {
+    console.error("Error deleting exercise:", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+
+  
 });
 
 module.exports = exerciseApi;
